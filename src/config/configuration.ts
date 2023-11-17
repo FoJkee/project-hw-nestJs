14ab69c5b48edaceabbfa0801c09947ff2e76b09
FoJkee <romanovsky0815@gmail.com>
2023-11-17 11:28:29 +0300

    fix 21

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,9 @@ dotenv.config();
 
 export default () => ({
   PORT: process.env.PORT || 4000,
+
   mongo_uri: process.env.MONGO_DB || 'mongodb://127.0.0.1:27017/hw',
+
   jwt_access_secret: process.env.JWT_ACCESS_SECRET || '123',
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET || '123',
   EMAIL: process.env.EMAIL,
