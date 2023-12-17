1f8e7cfe3e1963d63ae79e072a7fb7d5bcc56dd3
FoJkee <romanovsky0815@gmail.com>
2023-12-17 22:25:44 +0300

    fix test 6

--- test/post.e2e-spec.ts
+++ test/post.e2e-spec.ts
@@ -27,13 +27,6 @@ describe('posts', () => {
     server = app.getHttpServer();
     testingBlog = new TestingBlog(server);
     testingPost = new TestingPost(server);
-
-    app = module.createNestApplication();
-    app = createApp(app);
-    await app.init();
-    server = app.getHttpServer();
-    testingBlog = new TestingBlog(server);
-    testingPost = new TestingPost(server);
   });
 
   afterAll(async () => {
@@ -49,21 +42,113 @@ describe('posts', () => {
   });
 
   describe('POST', () => {
-    it('incorrect data', async () => {});
+    it('no data available posts, 400', async () => {
+      const errors = {
+        errorsMessages: [
+          {
+            message: expect.any(String),
+            field: 'title',
+          },
+          {
+            message: expect.any(String),
+            field: 'shortDescription',
+          },
+          {
+            message: expect.any(String),
+            field: 'content',
+          },
+          {
+            message: expect.any(String),
+            field: 'blogId',
+          },
+        ],
+      };
+
+      await request(server)
+        .post('/posts')
+        .auth('admin', 'qwerty')
+        .send({})
+        .expect(400, errors);
+
+      const response = await request(server).get('/posts');
+      expect(response.body.items).toEqual([]);
+      // expect(response.body).toStrictEqual(errors);
+    });
+
+    it('data is empty posts, 400', async () => {
+      const newPost = {
+        title: '',
+        shortDescription: '',
+        content: '',
+        blogId: '',
+      };
+      const errors = {
+        errorsMessages: [
+          {
+            message: expect.any(String),
+            field: 'title',
+          },
+          {
+            message: expect.any(String),
+            field: 'shortDescription',
+          },
+          {
+            message: expect.any(String),
+            field: 'content',
+          },
+          {
+            message: expect.any(String),
+            field: 'blogId',
+          },
+        ],
+      };
+
+      await request(server)
+        .post('/posts')
+        .auth('admin', 'qwerty')
+        .send(newPost)
+        .expect(400, errors);
+
+      const response = await request(server).get('/posts');
+
+      expect(response.body.items).toEqual([]);
+      // expect(response.body).toStrictEqual(errors);
+    });
     it('create blog correct data, 200', async () => {
       newBlog = await testingBlog.createBlog();
       const response = await request(server).get('/blogs');
       expect(response.body.items).toEqual([newBlog]);
     });
-    it('create post correct data', async () => {
+    it('create post correct data, 200', async () => {
       newPost1 = await testingPost.createPost(newBlog);
       const response = await request(server).get('/posts');
       expect(response.status).toBe(200);
     });
-    it('create post correct data', async () => {
+    it('create post correct data, 200', async () => {
       newPost2 = await testingPost.createPost(newBlog);
       const response = await request(server).get('/posts');
       expect(response.status).toBe(200);
     });
+  });
+  describe('GET', () => {
+    it('', async () => {});
+  });
+  describe('DELETE', () => {
+    it('', async () => {});
+  });
+  describe('PUT', () => {
+    it('', async () => {});
+  });
+  describe('GET => :id', () => {
+    it('', async () => {});
+  });
+  describe('POST => :id/comments', () => {
+    it('', async () => {});
+  });
+  describe('GET => :id/comments', () => {
+    it('', async () => {});
+  });
+  describe('PUT => id/like-status', () => {
+    it('', async () => {});
   });
 });
