b1337b5209032cbeac0834a17837d800def4f8fb
FoJkee <romanovsky0815@gmail.com>
2023-12-21 17:07:55 +0300

    fix test 11

--- src/config/throttle.config.ts
+++ src/config/throttle.config.ts
@@ -1,13 +1,11 @@
 import { ThrottlerOptionsFactory } from '@nestjs/throttler';
 import { ConfigService } from '@nestjs/config';
 import { ThrottlerModuleOptions } from '@nestjs/throttler/dist/throttler-module-options.interface';
-import * as process from 'process';
-
 export class ThrottlerConfigService implements ThrottlerOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
   //
-  private ttl = this.configService?.get<number>('throttler_ttl');
-  private limit = this.configService?.get<number>('throttler_limit');
+  // private ttl = this.configService?.get<number>('throttler_ttl');
+  // private limit = this.configService?.get<number>('throttler_limit');
 
   // private ttl = process.env.THROTTLE_TTL;
   // private limit: number = parseInt('throttler_limit', 10);
